87480473a38e6685af2e4b7fb7b5a899b0aca569
Luckas <luckasranarison@gmail.com>
2023-07-08 10:01:25 +0300

    update: newsfeed UI fix

--- client/src/components/MenuItem.jsx
+++ client/src/components/MenuItem.jsx
@@ -4,7 +4,7 @@ const MenuItem = ({ icon: Icon, label, onSelect }) => (
     className="flex items-center text-gray-1 hover:text-purple"
   >
     <Icon className="mr-2" />
-    <span className="font-semibold">{label}</span>
+    <span className="font-semibold hidden md:block">{label}</span>
   </button>
 );
 
