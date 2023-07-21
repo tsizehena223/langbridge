fbe1985f3b189c6026c33d5761e5d7d0a0596f6b
tsizehena223 <tsizehena223@gmail.com>
2023-07-21 11:26:09 +0300

    Bidy

--- server/src/Controller/SearchDataController.php
+++ server/src/Controller/SearchDataController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Service\DecodeJwt;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -11,10 +13,32 @@ use Symfony\Component\Routing\Annotation\Route;
 class SearchDataController extends AbstractController
 {
     #[Route(path: "/api/search", name: "app_search", methods: ["GET"])]
-    public function search(Request $request, EntityManagerInterface $em): JsonResponse
+    public function search(Request $request, EntityManagerInterface $em, DecodeJwt $decodeJwt): JsonResponse
     {
-        $keywords = $request->query->get("keywords");
+        $nationality = $request->query->get("nationality");
+        $number = $request->query->get("number");
+        $number = (null === $number) ? "5" : $number;
 
-        return new JsonResponse();
+        if (null === $nationality) {
+            return new JsonResponse();
+        }
+
+        $userThatShouldNotBeSent = $request->headers->get("authorization");
+        $userThatShouldNotBeSentId = $decodeJwt->getIdToken($userThatShouldNotBeSent);
+
+        if (!$userThatShouldNotBeSentId) {
+            return new JsonResponse(["message" => "User not authentified"], 403);
+        }
+
+        $qb = $em->createQueryBuilder();
+        $users = $qb->select("user.id", "user.username as name", "user.nationality as country")
+            ->from(User::class, "user")
+            ->where($qb->expr()->not($qb->expr()->eq("user.id", $userThatShouldNotBeSentId)))
+            ->andWhere($qb->expr()->eq("user.nationality", "'$nationality'"))
+            ->setMaxResults($number)
+            ->getQuery()
+            ->getResult();
+
+        return new JsonResponse($users);
     }
 }
