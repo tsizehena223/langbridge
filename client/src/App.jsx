d7aef6617840cff18a6bfba09625ea117789fff4
Luckas <luckasranarison@gmail.com>
2023-07-25 18:45:30 +0300

    add: basic auth handling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
-import { Home, Login, Signup, NotFound } from "./pages";
+import { useAuth } from "./contexts/AuthContext";
+import { Home, Login, Signup, NotFound, Newsfeed } from "./pages";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
+  const { isAuthentified } = useAuth();
+
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={isAuthentified ? <Newsfeed /> : <Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="*" element={<NotFound />} />
--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -6,24 +6,34 @@ import api from "../config/api";
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [userData, setUserData] = useState(localStorage.getItem("userdata"));
+  const decoded = useMemo(() => JSON.parse(localStorage.getItem("userdata")));
+  const [userData, setUserData] = useState(decoded);
+  const [isAuthentified, setIsAuthentified] = useState(
+    userData && userData.token
+  );
   const navigate = useNavigate();
 
   const login = async (formData) => {
     const res = await api.post("/users/login", formData);
     const data = res.data;
-    localStorage.setItem("userdata", data);
+    const json = JSON.stringify(data);
+    localStorage.setItem("userdata", json);
     setUserData(data);
+    setIsAuthentified(true);
     navigate("/");
   };
 
   const logout = () => {
     localStorage.removeItem("userdata");
     setUserData(null);
+    setIsAuthentified(false);
     navigate("/");
   };
 
-  const value = useMemo(() => ({ userData, login, logout }), [userData]);
+  const value = useMemo(
+    () => ({ userData, isAuthentified, login, logout }),
+    [userData]
+  );
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
