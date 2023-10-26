aae143f0edfc01246bfe45bbd1a56013cff28d60
Luckas <luckasranarison@gmail.com>
2023-10-26 07:42:54 +0300

    add: about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,34 @@
 import { MainLayout } from "../components";
+import Logo from "../assets/logo.svg";
+import IspmLogo from "../assets/logoISPM.jpg";
 
 const About = () => {
-  return <MainLayout>About</MainLayout>;
+  return (
+    <MainLayout>
+      <div
+        className="w-full p-8 flex flex-col items-center space-y-4
+        rounded-md bg-light dark:bg-gray-2"
+      >
+        <div className="flex space-x-8">
+          <img src={Logo} />
+          <img src={IspmLogo} />
+        </div>
+        <div className="text-purple text-3xl font-semibold">Langbridge</div>
+        <div className="text-center">
+          Langbridge is a social media where learners and native speakers come
+          together to exchange not just words, but stories, traditions, and
+          experiences. Whether you're a beginner or a seasoned polyglot,
+          LangBridge offers a space where everyone is welcomed, supported, and
+          encouraged to grow.
+        </div>
+        <div>
+          Made with ❤️ by{" "}
+          <span className="text-purple font-semibold">Team Langbridge</span> at
+          <span className="text-purple font-semibold"> ISPM</span>.
+        </div>
+      </div>
+    </MainLayout>
+  );
 };
 
 export default About;
